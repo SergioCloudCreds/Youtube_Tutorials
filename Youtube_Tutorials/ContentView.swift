9513b1a557c40f0df1e1927560c202324afd7d52
ItzzAxel <laurenttimburwa@gmail.com>
2026-08-05 21:07:58 +0200

    Initial Commit

---
+++ Youtube_Tutorials/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Youtube_Tutorials
+//
+//  Created by Ghost on 2026/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Youtube_Tutorials/Youtube_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_TutorialsApp.swift
+//  Youtube_Tutorials
+//
+//  Created by Ghost on 2026/08/05.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
