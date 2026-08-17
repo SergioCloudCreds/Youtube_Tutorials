bfcfa0321e00dd112746ae6711398ab26333e886
ItzzAxel <laurenttimburwa@gmail.com>
2026-08-17 09:04:22 +0200

    Added a new view to learn shapes for design, going to design the home page of the burger menu, with text and shapes

---
+++ Youtube_Tutorials/ShapeView.swift
@@ -0,0 +1,22 @@
+//
+//  ShapeView.swift
+//  Youtube_Tutorials
+//
+//  Created by Ghost on 2026/08/17.
+//
+
+import SwiftUI
+
+struct ShapeView: View {
+    var body: some View {
+        Circle()
+            .foregroundColor(.green)
+       
+    }
+}
+
+struct ShapeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapeView()
+    }
+}
