762ee41f487e96c53f8fd2f715f66c1bbaab9e0f
ItzzAxel <laurenttimburwa@gmail.com>
2026-09-14 11:25:26 +0200

    Added SubView to the View ExtractSubViewsView

--- Youtube_Tutorials/ExtractSubViewsView.swift
+++ Youtube_Tutorials/ExtractSubViewsView.swift
@@ -13,20 +13,41 @@ struct ExtractSubViewsView: View {
             //background
             Color(.brown).edgesIgnoringSafeArea(.all)
              
-            //Content
-            VStack{
-                Text("My Item")
-                Text("Apples")
-            }
-            .padding()
-            .background(Color.red)
-            .cornerRadius(10)
+            //content
+            contentLayer
             }
         }
+    
+    var contentLayer: some View {
+        HStack{
+            //Content
+            MyItem(title: "Apple", count: 1, color: .red)
+            MyItem(title: "Orange's", count: 2, color: .orange)
+            MyItem(title: "Banana's", count: 3, color: .yellow)
+            }
+    }
+    
     }
 
 struct ExtractSubViewsView_Previews: PreviewProvider {
     static var previews: some View {
         ExtractSubViewsView()
+    }
+}
+
+struct MyItem: View {
+    
+    let title: String
+    let count: Int
+    let color: Color
+    
+    var body: some View {
+        VStack{
+            Text("\(count)")
+            Text(title)
+        }
+        .padding()
+        .background(color)
+        .cornerRadius(10)
     }
 }
